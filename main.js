134cf836861e37daecfbf10f9d9f55db452108da
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-10-08 14:31:21 +0200

    Added fileupload jquery plugin as AMD dependency

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-define(function() {
+define([
+        "/uploader/js/jquery.fileupload-ui.js",
+        "/uploader/js/locale.js"
+    ], function() {
 
     var self;
 
     function init(config) {
         self = this;
+
+        // Initialize the jQuery File Upload widget:
+        $('#fileupload').fileupload();
+
+        // Enable iframe cross-domain access via redirect option:
+        $('#fileupload').fileupload(
+            'option',
+            'redirect',
+            window.location.href.replace(
+                /\/[^\/]*$/,
+                '/cors/result.html?%s'
+            )
+        );
+
+        // Load existing files:
+        $('#fileupload').each(function () {
+            var that = this;
+            $.getJSON(this.action, function (result) {
+                if (result && result.length) {
+                    $(that).fileupload('option', 'done')
+                        .call(that, null, {result: result});
+                }
+            });
+        });
     }
 
     return init;
